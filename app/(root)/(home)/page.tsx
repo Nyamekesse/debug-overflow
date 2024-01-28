adcec0cd8965f93541967bead62310ce0dec6a84
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-01-28 15:36:37 +0000

    added and completed home page components

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,62 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+
+const questions = [
+  {
+    _id: "1",
+    title: "Cascading Delete in SQL Alchemy",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
+    ],
+    author: { _id: "authorId1", name: "John Doe", picture: "url-to-picture" },
+    upvotes: 89,
+    views: 952,
+    answers: [],
+    createdAt: new Date("2023-09-01T12:00:00.00Z"),
+  },
+  {
+    _id: "2",
+    title: "How to center a div",
+    tags: [
+      { _id: "1", name: "css" },
+      { _id: "2", name: "html" },
+    ],
+    author: {
+      _id: "authorId2",
+      name: "Michael Patrick",
+      picture: "url-to-picture",
+    },
+    upvotes: 1000,
+    views: 1250459,
+    answers: [],
+    createdAt: new Date("2022-09-01T12:00:00.00Z"),
+  },
+  {
+    _id: "3",
+    title: "Node models not updating.",
+    tags: [
+      { _id: "1", name: "javascript" },
+      { _id: "2", name: "node js" },
+      { _id: "3", name: "web" },
+    ],
+    author: {
+      _id: "authorId3",
+      name: "Jones Aimee",
+      picture: "url-to-picture",
+    },
+    upvotes: 100,
+    views: 12514,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.00Z"),
+  },
+];
 
 export default function Home() {
   return (
@@ -31,6 +84,32 @@ export default function Home() {
         />
       </div>
       <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6 ">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question.id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no questions to show"
+            description="Be the first to break the silence! Ask a question and kickstart the
+        discussion. Your query could be the next big thing others learn from.
+        Get involved."
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
