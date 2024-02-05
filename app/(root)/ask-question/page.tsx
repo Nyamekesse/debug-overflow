8d3eb4d3da61157b7776cc759b80ed2b0e0ea147
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-05 14:51:38 +0000

    added a request to fetch user by id

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,14 +1,23 @@
+import { getUserById } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
 import Question from "../../../components/forms/Question";
 
-const page = () => {
+const Page = async () => {
+  // const { userId } = auth();
+  const userId = "CL123456";
+  if (!userId) redirect("/sign-in");
+
+  const mongoUser = await getUserById({ userId });
+  console.log(mongoUser);
+
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
       <div className="mt-9">
-        <Question mongoUserId="" type="" />
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} type="" />
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
