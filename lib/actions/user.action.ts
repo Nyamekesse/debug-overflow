244c4dd2407422383e297fa235d00029d1485ec9
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-06 17:08:20 +0000

    added actions for deleteUser, updateUser and createUser

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,14 @@
 "usr server";
 
+import Question from "@/database/question.model";
 import User from "@/database/user.model";
+import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../db.connection";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
 
 export async function getUserById(params: any) {
   try {
@@ -9,6 +16,49 @@ export async function getUserById(params: any) {
     const { userId } = params;
     const user = await User.findOne({ clerkId: userId });
     return user;
+  } catch (error) {
+    console.log("an error occurred", error);
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log("an error occurred", error);
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    const { clerkId, path, updateData } = params;
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    revalidatePath(path);
+  } catch (error) {
+    console.log("an error occurred", error);
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("user not found");
+    }
+    const userQuestions = await Question.find({ author: user._id }).distinct(
+      "_id"
+    );
+
+    await Question.deleteMany({ author: user._id });
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deletedUser;
   } catch (error) {
     console.log("an error occurred", error);
   }
