3e5107e09f79b4145d17bcb7b6a8ac9817e01dad
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-06 13:04:32 +0000

    called get all function to fetch questions from database

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,60 +5,12 @@ import NoResult from "@/components/shared/NoResult";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
+import { getQuestions } from "@/lib/actions/question.action";
 import Link from "next/link";
 
-const questions = [
-  {
-    _id: "1",
-    title: "Cascading Delete in SQL Alchemy",
-    tags: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: { _id: "authorId1", name: "John Doe", picture: "url-to-picture" },
-    upvotes: 89,
-    views: 952,
-    answers: [],
-    createdAt: new Date("2023-09-01T12:00:00.00Z"),
-  },
-  {
-    _id: "2",
-    title: "How to center a div",
-    tags: [
-      { _id: "1", name: "css" },
-      { _id: "2", name: "html" },
-    ],
-    author: {
-      _id: "authorId2",
-      name: "Michael Patrick",
-      picture: "url-to-picture",
-    },
-    upvotes: 1000,
-    views: 1250459,
-    answers: [],
-    createdAt: new Date("2022-09-01T12:00:00.00Z"),
-  },
-  {
-    _id: "3",
-    title: "Node models not updating.",
-    tags: [
-      { _id: "1", name: "javascript" },
-      { _id: "2", name: "node js" },
-      { _id: "3", name: "web" },
-    ],
-    author: {
-      _id: "authorId3",
-      name: "Jones Aimee",
-      picture: "url-to-picture",
-    },
-    upvotes: 100,
-    views: 12514,
-    answers: [],
-    createdAt: new Date("2021-09-01T12:00:00.00Z"),
-  },
-];
+export default async function Home() {
+  const result = await getQuestions({});
 
-export default function Home() {
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -85,8 +37,8 @@ export default function Home() {
       </div>
       <HomeFilters />
       <div className="mt-10 flex w-full flex-col gap-6 ">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
