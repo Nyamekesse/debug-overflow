1c531540f83aad0462d784a27dc83d88207aa0a4
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-06 13:02:52 +0000

    added get all questions method

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -2,31 +2,53 @@
 
 import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
+import User from "@/database/user.model";
+import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../db.connection";
+import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
 
-export async function createQuestion(params: any) {
+export async function getQuestions(params: GetQuestionsParams) {
+  try {
+    connectToDatabase();
+    const questions = await Question.find({})
+      .populate({
+        path: "tags",
+        model: Tag,
+      })
+      .populate({ path: "author", model: User })
+      .sort({ createdAt: -1 });
+    return { questions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createQuestion(params: CreateQuestionParams) {
   // eslint-disable-next-line no-empty
   try {
     connectToDatabase();
-    const {title, content, tags, author, path} = params
+    const { title, content, tags, author, path } = params;
 
     const question = await Question.create({
-      title, content, author
-    })
-    const tagDocuments =[]
-    for(const tag of tags){
+      title,
+      content,
+      author,
+    });
+    const tagDocuments = [];
+    for (const tag of tags) {
       const existingTag = await Tag.findOneAndUpdate(
         { name: { $regex: new RegExp(`^${tag}$`, "i") } },
         { $setOnInsert: { name: tag }, $push: { questions: question._id } },
         { upsert: true, new: true }
       );
-      tagDocuments.push(existingTag._id)
+      tagDocuments.push(existingTag._id);
     }
 
     await Question.findByIdAndUpdate(question._id, {
       $push: { tags: { $each: tagDocuments } },
     });
-    
+    revalidatePath(path);
   } catch (error) {
     console.log("an error occurred", error);
   }
