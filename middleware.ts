edee42975190478be0d0eeabacdd22b8fa6dd221
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-07 16:13:43 +0000

    added all routes to be ignored adn not protected

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,18 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/api/webhook/clerk",
+    "/api/rapidapi",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook/clerk", "/api/openai", "/api/rapidapi"],
 });
 
 export const config = {
