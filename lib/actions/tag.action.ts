c8119f0d95b6956a5feb5ad66ecf649e7e457dbc
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-07 20:04:05 +0000

    added getTopInteractedTags function to fetch tags interacted by a user

---
+++ lib/actions/tag.action.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { GetTopInteractedTagsParams } from "@/lib/actions/shared.types";
+import { connectToDatabase } from "@/lib/db.connection";
+import User from "@/database/user.model";
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    await connectToDatabase();
+    const { userId } = params;
+    const user = await User.findById(userId);
+    if (!user) throw new Error("User was found");
+    return [
+      { _id: "1", name: "tag1" },
+      { _id: "2", name: "tag2" },
+      { _id: "3", name: "tag3" },
+    ];
+  } catch (error) {
+    console.log("an error occurred", error);
+    throw error;
+  }
+}
