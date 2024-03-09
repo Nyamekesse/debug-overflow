4e10ad37dcffaf16943068265e3d782eed97e6aa
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-09 18:46:07 +0000

    added create answer function

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -1,17 +1,26 @@
 'use client';
 
 import { useTheme } from '@/context/ThemeProvider';
+import { createAnswer } from '@/lib/actions/answer.action';
 import { AnswerSchema } from '@/lib/validations';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Editor } from '@tinymce/tinymce-react';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 import { useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { Button } from '../ui/button';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from '../ui/form';
 
-const Answer = () => {
+interface Props {
+  question: string;
+  questionId: string;
+  authorId: string;
+}
+
+const Answer = ({ authorId, questionId, question }: Props) => {
+  const pathName = usePathname();
   const [isSubmitting, seIsSubmitting] = useState(false);
   const editorRef = useRef(null);
   const { mode } = useTheme();
@@ -21,12 +30,32 @@ const Answer = () => {
       answer: '',
     },
   });
-  const handleCreateAnswer = (data: any) => {};
+  const handleCreateAnswer = async (values: z.infer<typeof AnswerSchema>) => {
+    seIsSubmitting(true);
+    try {
+      await createAnswer({
+        author: JSON.parse(authorId),
+        content: values.answer,
+        question: JSON.parse(questionId),
+        path: pathName,
+      });
+
+      form.reset();
+      if (editorRef.current) {
+        const editor = editorRef.current as any;
+        editor.setContent('');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      seIsSubmitting(false);
+    }
+  };
   return (
     <div>
       <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
         <h4 className="paragraph-semibold text-dark400_light800">Write your answer here.</h4>
-        <Button className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500" onClick={()=>{}}>
+        <Button className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500" onClick={() => {}}>
           <Image src="/assets/icons/stars.svg" alt="star" width={12} height={12} className="object-contain" />
           Generate an AI Answer
         </Button>
@@ -86,7 +115,7 @@ const Answer = () => {
             )}
           />
           <div className="flex justify-end">
-            <Button className="primary-gradient w-fit text-white" type="button" disabled={isSubmitting}>
+            <Button className="primary-gradient w-fit text-white" type="submit" disabled={isSubmitting}>
               {isSubmitting ? 'Submitting...' : 'Submit'}
             </Button>
           </div>
