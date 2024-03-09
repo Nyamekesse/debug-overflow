7bce2aafd03ea3f8d8086f3e0b3fdee617550f26
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-09 20:26:15 +0000

    added function to fetch all answers

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -5,7 +5,7 @@ import Question from '@/database/question.model';
 import User from '@/database/user.model';
 import { revalidatePath } from 'next/cache';
 import { connectToDatabase } from '../db.connection';
-import { CreateAnswerParams } from './shared.types';
+import { CreateAnswerParams, GetAnswersParams } from './shared.types';
 
 export async function createAnswer(params: CreateAnswerParams) {
   connectToDatabase();
@@ -35,5 +35,17 @@ export async function createAnswer(params: CreateAnswerParams) {
   } catch (error) {
     console.log(error);
     throw error;
+  }
+}
+
+export async function getAnswers(params: GetAnswersParams) {
+  try {
+    connectToDatabase();
+    const { questionId } = params;
+    const answers = await Answer.find({ question: questionId }).populate('author', '_id clerkId name picture').sort({ createdAt: -1 });
+
+    return { answers };
+  } catch (error) {
+    console.log(error);
   }
 }
