53baf33c154e3346ebcf1f49cdee38e8903a3568
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-10 21:26:15 +0000

    added and completed vote component

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import { UserId, Voting } from '@/lib/actions/shared.types';
+import { getFormattedNumber } from '@/lib/utils';
+import Image from 'next/image';
 
 interface Props extends UserId, Voting {
   type: string;
@@ -8,8 +12,50 @@ interface Props extends UserId, Voting {
   hasSaved?: boolean;
 }
 
+const handleVote = (action: string) => {};
+const handleSave = () => {};
+
 const Votes = ({ type, itemId, userId, upvotes, hasupVoted, downvotes, hasdownVoted, hasSaved }: Props) => {
-  return <div>Votes</div>;
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            className="cursor-pointer"
+            src={hasupVoted ? '/assets/icons/upvoted.svg' : '/assets/icons/upvote.svg'}
+            width={18}
+            height={18}
+            alt="vote icon"
+            onClick={() => handleVote('upvote')}
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] p-1 rounded-sm ">
+            <p className="subtle-medium text-dark400_light900">{getFormattedNumber(upvotes)}</p>
+          </div>
+        </div>
+        <div className="flex-center gap-1.5">
+          <Image
+            className="cursor-pointer"
+            src={hasdownVoted ? '/assets/icons/downvoted.svg' : '/assets/icons/downvote.svg'}
+            width={18}
+            height={18}
+            alt="vote icon"
+            onClick={() => handleVote('downvote')}
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] p-1 rounded-sm ">
+            <p className="subtle-medium text-dark400_light900">{getFormattedNumber(downvotes)}</p>
+          </div>
+        </div>
+      </div>
+      <Image
+        className="cursor-pointer"
+        src={hasSaved ? '/assets/icons/star-filled.svg' : '/assets/icons/star-red.svg'}
+        width={18}
+        height={18}
+        alt="save icon"
+        onClick={handleSave}
+      />
+    </div>
+  );
 };
 
 export default Votes;
