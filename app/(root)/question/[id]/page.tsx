c581e366b5d5e113faf225c3b0c0bc7df1c15741
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-10 21:27:00 +0000

    added vote component to question details

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -3,6 +3,7 @@ import AllAnswers from '@/components/shared/AllAnswers';
 import Metric from '@/components/shared/Metric';
 import ParseHtml from '@/components/shared/ParseHTML';
 import RenderTags from '@/components/shared/RenderTags';
+import Votes from '@/components/shared/Votes';
 import { getQuestionById } from '@/lib/actions/question.action';
 import { getUserById } from '@/lib/actions/user.action';
 import { getFormattedNumber, getTimestamp } from '@/lib/utils';
@@ -26,7 +27,18 @@ async function Page({ params, searchParams }) {
             <Image src={result.author.picture} alt={result.author.name} className={'rounded-full'} width={22} height={22} />
             <p className={'paragraph-semibold text-dark300_light700'}>{result.author.name}</p>
           </Link>
-          <div className={'flex justify-end'}>VOTING</div>
+          <div className={'flex justify-end'}>
+            <Votes
+              type="Question"
+              itemId={JSON.stringify(result._id)}
+              userId={JSON.stringify(mongoUser._id)}
+              upvotes={result.upvotes.length}
+              hasupVoted={result.upvotes.includes(mongoUser._id)}
+              downvotes={result.downvotes.length}
+              hasdownVoted={result.downvotes.includes(mongoUser._id)}
+              hasSaved={mongoUser?.saved.includes(result._id)}
+            />
+          </div>
         </div>
         <h2 className={'h2-semibold text-dark200_light900 mt-3.5 w-full text-left'}>{result.title}</h2>
       </div>
